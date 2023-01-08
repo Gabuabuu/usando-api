3a46ef866df6fc2e8a244d5426f721b448e628bb
Gabuabuu <gabydavilaneves@gmail.com>
2023-01-08 17:03:00 -0300

    Começando a utilizar a api do back no frontend

---
+++ frontend/index.js
@@ -0,0 +1,13 @@
+//const { response, json } = require("express");
+
+ async function getContent () {
+   try {
+    const response = await fetch('http://localhost:2811/')
+   } catch (error) {
+        console.log("ERROOOOOAAAAAA")
+   }
+ }
+
+   
+
+

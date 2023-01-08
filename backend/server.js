beb04ec0fd1a7c689143556e2b155b86d99d3b59
Gabuabuu <gabydavilaneves@gmail.com>
2023-01-08 16:17:57 -0300

    Configurando 'server' de testes

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const cors = require('cors');
+const express = require('express');
+const app = express();
+const axios = require('axios');
+
+app.get('/',  (req, res) => {
+    return res.json({ message: 'Okay' })
+})
+
+app.listen('2811') //Limite de numeros para a porta observar é de '5'??
